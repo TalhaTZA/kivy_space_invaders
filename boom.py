b0388a8b4e880c993ac667aa597c36f1348ea922
TalhaTZA <talhavjtza@gmail.com>
2017-11-20 23:12:19 +0500

    till ammo class

---
+++ boom.py
@@ -0,0 +1,10 @@
+from kivy.uix.image import Image
+from kivy.core.audio import SoundLoader
+
+
+class Boom(Image):
+    sound=SoundLoader.load('boom.wav')
+    
+    def boom(self,**kwargs):
+        self.__class__.sound.play()
+        super().__init__(**kwargs)
